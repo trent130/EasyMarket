41668aee3712a297762b19003a83d00c409bd400
trent130 <warsamegift@gmail.com>
2024-01-30 16:20:57 +0300

    Update templates and URLs

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('faq/', views.faq, name='faq'),
+    path('help/', views.help, name='help'),
+    path('signin/', views.signin, name='signin'),
+    path('register/', views.register, name='register'),
+    path('search/', views.search, name='search'),
+    path('chat/', views.chat, name='chat'),
+    # path('chat/<str:room_name>/', views.room, name='room'),
+    path('categories/', views.categories, name='categories'),
+    path('products/', views.products, name='products'),
+    path('orders/', views.orders, name='orders'),
 ]
--- staticpages/views.py
+++ staticpages/views.py
@@ -2,13 +2,50 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    context = {'title': 'home'}
     return render(request, 'staticpages/index.html')
 
 def about(request):
+    context: {'title' : 'about'}
     return render(request, 'staticpages/about.html')
 
 def contact(request):
+    context = {'title': 'contact'}
     return render(request, 'staticpages/contact.html')
 
-def faq(request):
-    return render(request, 'staticpages/faq.html')
+def help(request):
+    context = {'title': 'help'}
+    return render(request, 'staticpages/help.html')
+
+def signin(request):
+    context = {'title': 'signin'}
+    return render(request, 'marketplace/account/register.html')
+
+def register(request):
+    context = {'title': 'register'}
+    return render(request, 'marketplace/account/register.html')
+
+def search(request):
+    context = {'title': 'search'}
+    return render(request, 'marketplace/search.html')
+
+def chat(request):
+    context = {'title': 'chat'}
+    return render(request, 'staticpages/chat.html')
+
+def categories(request):
+    context = {'title': 'categories'}
+    return render(request, 'marketplace/categories.html')
+
+# def room(request, room_name):
+#     return render(request, 'staticpages/room.html', {
+#         'room_name': room_name
+#     })
+
+def products(request):
+    context = {'title': 'products'}
+    return render(request, 'products/product.html')
+
+def orders(request):
+    context = {'title': 'orders'}
+    return render(request, 'orders/order.html')
