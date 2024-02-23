687bd53e00af3593b3fe205478a1f58d4d0f7f5a
trent130 <warsamegift@gmail.com>
2024-02-23 10:35:51 +0300

    updated the reggister forms

--- staticpages/forms.py
+++ staticpages/forms.py
@@ -6,14 +6,18 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-class RegistrationForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-    password  = forms.CharField(widget = forms.PasswordInput)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     phone_number = forms.CharField(max_length=15, required=False)
     address = forms.CharField(max_length=100, required=False)
-    role = forms.ChoiceField(choices=[('buyer', 'Buyer'), ('seller', 'Seller'), ('both', 'Both')])
+    role_choices = [
+        ('buyer', 'Buyer'),
+        ('seller', 'Seller'),
+        ('both', 'Both'),
+    ]
+    role = forms.ChoiceField(choices=role_choices)
 
     class Meta:
         model = User
