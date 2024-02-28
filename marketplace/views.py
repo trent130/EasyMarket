0e5e2914aac220f729dd9d4d724c37eb78a81940
trent130 <warsamegift@gmail.com>
2024-02-28 18:20:30 +0300

    Update cart styling

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,59 @@
-# from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
-# from django.http import JsonResponse
-# from channels.layers import get_channel_layer
-# from asgiref.sync import async_to_sync
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import redirect
+from orders.models import Order
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import redirect
+
+@login_required
+def checkout(request):
+    if request.method == 'POST':
+        user = request.user
+
+        # Create a new order
+        order = Order(user=user)
+
+        # Add the items from the cart to the order
+        for item in user.cart.all():
+            order.items.add(item)
+
+        # Save the order
+        order.save()
+
+        # Clear the cart
+        user.cart.clear()
+        user.save()
+
+        messages.success(request, "Checkout successful. Your order is being processed.")
+        return redirect('marketplace:order_confirmation', order_id=order.id)
+
+    else:
+        # If the request method is not POST, redirect to the cart page
+        return redirect('marketplace:cart')
+
+def clear_cart(request): 
+    if request.method == 'POST':
+        user = request.user
+        user.cart.clear()
+        user.save()
+        return redirect('marketplace:cart_cleared')
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+def cart(request):
+    return render(request, 'marketplace/cart.html')
+
+def cart_cleared(request):
+    return render(request, 'marketplace/cart_cleared.html')
+
+
+
+
+
+
+
+
 # from .models import Message, Reaction, UserProfile
 # from django.views.decorators.csrf import csrf_exempt
 # from django.http import HttpResponse
@@ -87,7 +138,3 @@
 
 #         return JsonResponse({'status': 'success'})
 
-def checkout(request):
-    if request.method =='POST':
-        user = request.user
-        
