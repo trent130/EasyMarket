a5a2ab2a8e1fbfd0de9f33336874a38122773571
trent130 <warsamegift@gmail.com>
2024-03-02 14:40:40 +0300

    Fix database column error and update email settings

--- students/settings.py
+++ students/settings.py
@@ -107,8 +107,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'greywool@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'reedrichards@20000' # Replace with your email password
+EMAIL_HOST_USER = 'greywolfextreme@gmail.com'  
+EMAIL_HOST_PASSWORD = 'Larrymax9521' 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
