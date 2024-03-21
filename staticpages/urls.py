5801762757395aa3c4590f43dd58920cb62cc9be
trent130 <warsamegift@gmail.com>
2024-03-21 18:28:43 +0300

    Add category functionality and update navigation menu

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(), name = 'password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
     path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_completee'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_completee'), path('add_category/', views.add_category, name='add_category'),
 ]
+
+    
