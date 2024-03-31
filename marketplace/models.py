947c1c0644075d451adacea0b45ca2b3337756d9
trent130 <warsamegift@gmail.com>
2024-03-31 16:29:22 +0300

    Fix binary file differences and update search functionality

--- marketplace/models.py
+++ marketplace/models.py
@@ -6,7 +6,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 @receiver(post_save, sender=User)
 def create_student_profile(sender, instance, created, **kwargs):
     if created:
@@ -32,6 +31,15 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username}'
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+    
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
