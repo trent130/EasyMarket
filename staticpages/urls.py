34606f054e8d66c1a1f51bd86e6287eaa0b5f543
trent130 <warsamegift@gmail.com>
2024-03-31 23:37:08 +0300

    Update models and views, fix CSS styles, and add category_products view

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('add_category/', views.add_category, name='add_category'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_completee'), path('add_category/', views.add_category, name='add_category'),
     path('account/profile', views.user_profile, name= 'profile'),
+    path('category/<int:category_id>/products/', views.category_products, name='category_products'),
 ]
 
     
