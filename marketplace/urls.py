38b76f9f451999f22f8ce47e255b6e7747712a13
trent130 <warsamegift@gmail.com>
2024-06-20 21:36:39 +0300

    feat: add wishlist functionality

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -7,9 +7,12 @@ app_name = 'marketplace'
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('search/', views.search, name='search'),
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart, name='cart'),
-    path('clear_cart', views.clear_cart, name="clear_cart"),
-    path('update_cart/<int:product_id>/', views.update_cart, name='update_cart'),
-    path('remove_from_cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/clear/', views.clear_cart, name="clear_cart"),
+    path('cart/update/<int:product_id>/', views.update_cart, name='update_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('wishlist', views.wishlist, name='wishlist'),
+    path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove/<int:wishlist_item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
