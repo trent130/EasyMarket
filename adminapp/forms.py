7eed498b65cc19662f011b389961d201c9663294
trent130 <warsamegift@gmal.com>
2024-06-29 04:31:21 +0300

    merge

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -3,5 +3,4 @@ from django import forms
 from django.contrib.auth.models import Group
 
 class UserRoleForm(forms.Form):
-    # role = forms.ChoiceField(choices=[(group.name, group.name) for group in Group.objects.all()])
-    pass
+    role = forms.ChoiceField(choices=[(group.name, group.name) for group in Group.objects.all()])
