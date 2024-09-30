51d2360c6bd07998e1df1188172c8f1ec9c41168
code diary <bytecodediary@gmail.com>
2024-09-30 21:33:54 +0300

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     path('adminapp/', include('adminapp.urls')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
