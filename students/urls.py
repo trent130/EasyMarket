5214659440fa496d4ae8b520452c0da405c5d9ab
trent130 <warsamegift@gmail.com>
2024-09-30 08:38:51 +0000

    refactor: apply miscellaneous updates

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r"^static/(?P<path>.*)", serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?<path>.*)', serve,  {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('staticpages.urls')),
     path('marketplace/', include('marketplace.urls')),
@@ -13,5 +16,5 @@ urlpatterns = [
     path('adminapp/', include('adminapp.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
