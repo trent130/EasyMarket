07d73d8e515bd1a7885fdf3a92f0bb16982e5171
Lawrence Wafula <72649310+trent130@users.noreply.github.com>
2024-09-30 08:38:51 +0000

    updates

--- students/settings.py
+++ students/settings.py
@@ -116,7 +116,7 @@ ROOT_URLCONF = 'students.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
