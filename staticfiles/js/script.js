b5b685565fdec5b132d56fdb3397be59eed37c74
trent130 <warsamegift@gmail.com>
2024-11-25 10:15:05 +0300

    removed the staticfiles directory

--- staticfiles/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.product-card').forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.querySelector('.product-reviews').style.display = 'block';
-    });
-    card.addEventListener('mouseleave', () => {
-        card.querySelector('.product-reviews').style.display = 'none';
-    });
-});
