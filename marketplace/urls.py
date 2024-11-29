28fb981f5942427fe8623959e8822cca082fd0c9
trent130 <warsamegift@gmail.com>
2024-11-29 15:52:17 +0300

    "Added WebSocket support to marketplace app, merged 2FA dependencies into main requirements.txt, and removed separate requirements_2fa.txt file."

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
+from marketplace import consumers
 from . import views
 
 app_name = 'marketplace'
@@ -18,4 +20,8 @@ urlpatterns = [
     path('wishlist', views.wishlist_view, name='wishlist'),
     path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:wishlist_item_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+]
+
+websocket_urlpatterns = [
+    path('ws/marketplace/', consumers.MarketplaceConsumer.as_asgi()),
 ]
