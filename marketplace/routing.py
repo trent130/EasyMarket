29257a4009993f39d46c9922684054dfc6d235a4
trent130 <warsamegift@gmail.com>
2024-12-03 11:19:06 +0300

    "Deleted various Python files and code blocks in backend/api, frontend/lib, marketplace, products, and students directories."

--- marketplace/routing.py
+++ marketplace/routing.py
@@ -5,8 +5,6 @@
 # from marketplace.consumers import ChatConsumer
 
 from django.urls import re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from . import consumers
 
 websocket_urlpatterns = [
