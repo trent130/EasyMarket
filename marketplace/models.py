478bf5ae4c6149d84640c840dfa0c41fb8267f6a
trent130 <warsamegift@gmail.com>
2024-12-05 13:12:44 +0300

    refactor: apply various code updates

--- marketplace/models.py
+++ marketplace/models.py
@@ -18,6 +18,13 @@ class Student(models.Model):
     bio = models.TextField(blank=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the student, which is
+        the combination of their first and last names.
+
+        Returns:
+            str: The string representation of the student.
+        """
         return f'{self.first_name} {self.last_name}'
 
 
@@ -26,6 +33,13 @@ class UserProfile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', default='avatars/default.jpg')
 
     def __str__(self):
+        """
+        Returns a string representation of the user profile, which is
+        the username of the associated user.
+
+        Returns:
+            str: The string representation of the user profile.
+        """
         return f'{self.user.username}'  # pylint: disable=no-member
 
 
@@ -65,6 +79,14 @@ class Message(models.Model):
     read = models.BooleanField(default=False)
 
     def __str__(self):
+        """
+        Returns a string representation of the message, which is
+        the combination of the associated user's username and the
+        timestamp of the message.
+
+        Returns:
+            str: The string representation of the message.
+        """
         return f'{self.user.username} - {self.timestamp}'
 
 
@@ -85,6 +107,13 @@ class Reaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the reaction, which includes
+        the type of reaction and the username of the user who made it.
+
+        Returns:
+            str: The string representation of the reaction.
+        """
         return f'{self.reaction_type} by {self.user.username}'
 
 
@@ -96,6 +125,14 @@ class Review(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the review, which includes
+        the title of the product being reviewed and the username of the
+        user who wrote the review.
+
+        Returns:
+            str: The string representation of the review.
+        """
         return f'Review for {self.product.title} by {self.reviewer.username}'
 
     class Meta:
@@ -121,12 +158,30 @@ class Cart(models.Model):
     slug = models.SlugField()
 
     def __str__(self):
+        """
+        Returns a string representation of the cart, which is
+        the username of the associated user.
+
+        Returns:
+            str: The string representation of the cart.
+        """
+    
         return f"Cart for {self.user.username}"
 
     def total_items(self):
+        """
+        Returns the total number of items in the cart.
+
+        """
         return sum(item.quantity for item in self.cartitem_set.all())
 
     def total_price(self):
+        """
+        Returns the total price of all items in the cart.
+
+        Returns:
+            float: The total price of all items in the cart.
+        """
         return sum(item.total_price() for item in self.cartitem_set.all())
 
 
@@ -137,9 +192,22 @@ class CartItem(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
+        """
+        Returns a string representation of the cart item, which includes
+        the quantity and title of the product in the cart.
+
+        Returns:
+            str: The string representation of the cart item.
+        """
         return f"{self.quantity} x {self.product.title} in cart"
 
     def total_price(self):
+        """
+        Returns the total price of the cart item, which is the product price times the quantity.
+
+        Returns:
+            float: The total price of the cart item.
+        """
         return self.product.price * self.quantity
 
 
@@ -166,4 +234,10 @@ class WishList(models.Model):
     auto_now = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the wishlist, which is the username of the associated user concatenated with the string "'s wishlist".
+        
+        Returns:
+            str: The string representation of the wishlist.
+        """
         return f"{self.user.username}'s wishlist"
