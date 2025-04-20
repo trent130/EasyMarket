0b52912b8e2f26ce514983ad533c93a2af853c3d
trent130 <warsamegift@gmail.com>
2025-04-20 15:12:34 +0300

    fixed a typo in marketplace views

--- marketplace/management/commands/find_duplicates.py
+++ marketplace/management/commands/find_duplicates.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from users.model import CustomUser
 from marketplace.models import WishList
 
-
 class Command(BaseCommand):
     help = "Find dublicate wishlist objects"
 
