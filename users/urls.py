59947575244310647fdd7008a705ae75d0b426b9
trent130 <warsamegift@gmail.com>
2025-05-28 23:08:24 +0300

    message

--- users/urls.py
+++ users/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('auth/forgot-password/', views.forgot_password, name='forgot_password'),
     path('auth/reset-password/', views.reset_password, name='reset_password'),
     path('auth/change-password/', views.change_password, name='change_password'),
+    path('auth/me/', views.check_authentication, name='check-authentication'),
 ]
 
